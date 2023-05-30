2415c949b9f058c87d7c58177ac2d75937bc378a
Adam Ironclad Wells <alwells408@gmail.com>
2023-05-30 19:14:51 -0400

    Fix matts number in resume and adjust scrollwatcher

--- src/app/components/ScrollWatcher.tsx
+++ src/app/components/ScrollWatcher.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { useEffect } from "react"
 import { throttle } from "lodash"
 
@@ -6,32 +7,29 @@ export default function ScrollWatcher() {
   // Top : 0, 150, 0
   // Mid : 255, 150, 0
   // Bot : 255, 0, 0
-  const gradients = [
-    [0, 150, 0],
-    [255, 150, 0],
-    [255, 0, 0]
-  ]
+  // console.log('window.innerHeight: ', window.innerHeight)
+  // console.log('window.scrollY: ', window.scrollY)
+  // console.log('document.body.offsetHeight: ', document.body.offsetHeight)
+  // console.log('First Calc: ', (window.innerHeight + Math.round(window.scrollY)))
 
-  // const rgbRanges = {
-  //   red: [],
-  //   green: [],
-  //   blue: []
-  // }
+  const setBgGradient = () => {
+    const percentage = (window.innerHeight + Math.round(window.scrollY)) / document.body.offsetHeight
+    // console.log('percentage: ', percentage)
+    document.documentElement.style.setProperty('--scrollbar-gradient', `
+      linear-gradient(
+        to bottom,
+        white 50%,
+        rgba(${255 * percentage}, ${150 - 150 * percentage}, 0)
+      )
+    `);
+  }
 
   useEffect(() => {
-    const styleRoot = getComputedStyle(document.documentElement) as unknown as HTMLElement
-
+    // Initial work on load
+    setBgGradient()
+    // Listener for adjustments
     const onScroll = () => {
-      const percentage = (window.innerHeight + Math.round(window.scrollY)) / document.body.offsetHeight
-      // console.log('top percentage: ', gradients[0][1] * percentage)
-      console.log('percentage: ', percentage)
-      document.documentElement.style.setProperty('--scrollbar-gradient', `
-        linear-gradient(
-          to bottom,
-          white 50%,
-          rgba(${255 * percentage}, ${150 - 150 * percentage}, 0)
-        )
-      `);
+      setBgGradient();
     }
 
     window.addEventListener('scroll', throttle(onScroll, 100))
